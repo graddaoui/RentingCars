a1152c16a508a6086930fca2b296a73bf11f7ea6
unknown <ghassen.raddaoui.512@gmail.com>
2023-01-18 10:32:45 +0100

    reverting the Security implementation

--- src/main/java/com/ghassen/rent/car/LocationApplication.java
+++ src/main/java/com/ghassen/rent/car/LocationApplication.java
@@ -1,17 +1,7 @@
 package com.ghassen.rent.car;
 
-import com.ghassen.rent.car.entities.User;
-import com.ghassen.rent.car.entities.Role;
-import com.ghassen.rent.car.services.UserService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.util.ArrayList;
 
 @SpringBootApplication
 public class LocationApplication {
@@ -20,10 +10,6 @@ public class LocationApplication {
 		SpringApplication.run(LocationApplication.class, args);
 	}
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-	}
 	}
 
 
